fcba0a25869b6c73fa8ca149f98a7112a5fad4a1
Guilherme Mafezoli <gmafezoli@protonmail.com>
2021-06-20 17:06:47 -0300

    Update monitors script condition

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
-if [[ $laptop_external_monitor = *connected* ]]; then
+if [[ $laptop_external_monitor != *disconnected* ]]; then
 	xrandr --output eDP-1 --primary --mode 1366x768 --rotate normal --output HDMI-1 --mode 1360x768 --rotate normal --left-of eDP-1
 fi
